f3756d7fd13b63666dda3bbca74f645b6cf365ea
pbarbarone <peter.barbarone@gmail.com>
2018-01-09 12:01:36 -0800

    practiced parsing data object. now to do oauth?

--- controllers/update.js
+++ controllers/update.js
@@ -1,15 +1,33 @@
 var express = require('express');
+var discogs = require('disconnect').Client;
 var passport = require('../config/passportConfiguration');
 var db = require('../models');
 var router = express.Router();
 
 
+//establishing update route
 router.get('/', function(req, res){
 	res.send('here you will search/upload albums to your library');
 })
 
+//workshopping api module
+var dbtest = discogs().database();
+dbtest.getRelease(26764, function(err, data){
+	var artistNames = data.artists.map(function(artist){
+		return artist.name;
+	})
+	var trackNames=[];
+	var allTracks = data.tracklist.forEach(function(tracklist){
+		trackNames.push(tracklist.title);
+		// console.log(trackNames);
+	})
+	console.log('mapped artist name', artistNames);
+	console.log('album tracks', trackNames);
+	console.log('album name', data.title);
+	console.log('album ID', data.id);
+	console.log(data.styles);
 
-
+});
 
 
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var flash = require('connect-flash'); 
 var isLoggedIn = require('./middleware/isLoggedIn');
+var discogs = require('disconnect');
 var passport = require('./config/passportConfiguration');
 var session = require('express-session');
 var app = express();
