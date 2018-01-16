a64b087f5449fc1eef5433c9409f475aa1907a83
pbarbarone <peter.barbarone@gmail.com>
2018-01-16 09:29:55 -0800

    removed unused module

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var flash = require('connect-flash'); 
 var isLoggedIn = require('./middleware/isLoggedIn');
-var discogs = require('disconnect');
 var passport = require('./config/passportConfiguration');
 var session = require('express-session');
 var app = express();
